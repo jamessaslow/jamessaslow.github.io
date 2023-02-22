539483cb3480fa1dd63d75caa37bddbc019fd7d5
James <jamessaslow@gmail.com>
2023-02-22 14:03:47 -0800

    Create dynamic.js

---
+++ dynamic.js
@@ -0,0 +1,36 @@
+const textArray = ["I'm a Physicist", "I'm a Programmer", "I'm a Data Scientist"];
+let textIndex = 0;
+let charIndex = 0;
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+const detel = document.querySelector(".detel");
+const textElement = document.querySelector(".detel h2");
+
+function type() {
+  if (charIndex < textArray[textIndex].length) {
+    textElement.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    textElement.textContent = textArray[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textIndex++;
+    if (textIndex >= textArray.length) {
+      textIndex = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, newTextDelay + 250);
+});
